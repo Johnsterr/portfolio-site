4de1a80ddff5d9f8097cb4e3065e20b63082d6e7
Evgeniy <evgpashko92@gmail.com>
2022-10-31 23:34:46 +0700

    Configuration project

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+import {defineNuxtConfig} from "nuxt/config";
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  css: ["~/assets/css/tailwind.css"],
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+  modules: ["@nuxtjs/tailwindcss"],
+  tailwindcss: {},
+});
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+import * as path from "path";
+
+const srcDir = path.join(__dirname); // without / on end
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: [
+    `${srcDir}/assets/**/*.css`,
+    `${srcDir}/components/**/*.vue`,
+    `${srcDir}/layouts/**/*.vue`,
+    `${srcDir}/pages/**/*.vue`,
+    `${srcDir}/composables/**/*.{vue,js,ts}`,
+    `${srcDir}/plugins/**/*.{js,ts}`,
+    `${srcDir}/*.{vue,js,ts}`,
+    `${srcDir}/nuxt.config.{js,ts}`,
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
