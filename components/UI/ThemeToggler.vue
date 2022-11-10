05b98d16da0617b4feda8d24b3ee26858fbd307a
Evgeniy <evgpashko92@gmail.com>
2022-11-10 23:55:34 +0700

    Компонент переключения темы

---
+++ components/UI/ThemeToggler.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button
+      class="h-6 w-[48px] rounded-[100px] bg-bg-primary p-1 transition-all"
+      @click="toggleTheme"
+    >
+      <span
+        class="shadow-inset-shadow/100 relative top-0 h-5 w-5 rounded-[50px] transition-all"
+      >
+      </span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { ThemeMode } from "~~/Domains/Entities/ThemeMode/ThemeMode";
+
+export default defineComponent({
+  setup() {
+    const colorMode = ThemeMode.create();
+
+    const toggleTheme = () => colorMode.toggleMode();
+
+    return {
+      toggleTheme,
+    };
+  },
+});
+</script>
