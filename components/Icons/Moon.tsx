79517afdc4a4af09ae5ab0d3770124754b011121
Evgeniy <evgpashko92@gmail.com>
2022-11-13 01:00:02 +0700

    Добавил иконки Солнца и Луны

---
+++ components/Icons/Moon.tsx
@@ -0,0 +1,23 @@
+import { defineComponent } from "vue";
+
+const Moon = defineComponent({
+  setup(props, context) {
+    return () => (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        class="h-5 w-5"
+        aria-hidden="true"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M7.455 2.004a.75.75 0 01.26.77 7 7 0 009.958 7.967.75.75 0 011.067.853A8.5 8.5 0 116.647 1.921a.75.75 0 01.808.083z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    );
+  },
+});
+
+export default Moon;
