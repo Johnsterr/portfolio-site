4184d64a56c7f23eb39a92d880e17db16bdb8198
Evgeniy <evgpashko92@gmail.com>
2022-11-19 23:21:00 +0700

    Интерфейс для сущности Режима темы

--- Domains/Entities/ThemeMode/IThemeMode.ts
+++ Domains/Entities/ThemeMode/IThemeMode.ts
@@ -2,6 +2,10 @@ import { ColorModeInstance } from "@nuxtjs/color-mode/dist/runtime/types";
 
 export interface IThemeMode {
   colorMode: ColorModeInstance;
+  darkMode: boolean;
+  // Getters
+  getColorMode: string;
+  isDarkMode: boolean;
+  // Methods
   toggleMode: () => void;
-  getMode: string;
 }
