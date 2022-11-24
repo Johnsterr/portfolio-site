9a0383436f10cac6bd7c00fea31bef2f1f537308
Evgeniy <evgpashko92@gmail.com>
2022-11-24 23:51:43 +0700

    Добавлен ключ для темы в настройку nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,15 @@
 import { defineNuxtConfig } from "nuxt/config";
+import { storageThemeModeKey } from "./application/config/StorageThemeModeKey";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ["~/assets/css/tailwind.css"],
-  build: {
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
-    },
-  },
+  build: {},
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
   tailwindcss: {},
   colorMode: {
     classSuffix: "",
     preference: "light",
-    storageKey: "color-mode",
+    storageKey: storageThemeModeKey,
   },
 });
