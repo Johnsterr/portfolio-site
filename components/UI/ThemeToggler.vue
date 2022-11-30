3f3c2cf512861b470a6dd44cf82fdf0ba3a9259d
Evgeniy <evgpashko92@gmail.com>
2022-11-30 23:47:07 +0700

    Кнопка переключения темы

--- components/UI/ThemeToggler.vue
+++ components/UI/ThemeToggler.vue
@@ -1,40 +1,34 @@
 <template>
-  <div>
-    <button
-      class="h-7 w-[52px] overflow-hidden rounded-[100px] bg-bg-primary p-1 transition-all"
-      @click="toggleTheme"
-    >
-      <span
-        class="relative top-0 h-5 w-5 transition-all"
-        :class="{
-          'left-[2px]': !active,
-          'text-yellow-500': !active,
-          'left-[22px]': active,
-          'text-white': active,
-        }"
-      >
-        <IconsSun v-if="!active" class="h-5 w-5" />
-        <IconsMoon v-else class="h-5 w-5" />
-      </span>
-    </button>
-  </div>
+  <button
+    class="h-7 w-[52px] overflow-hidden rounded-[100px] bg-bg-primary p-1 transition-all"
+    @click="toggleTheme"
+  >
+    <ThemeTogglerSpan :dark-mode-active="isDarkMode" />
+  </button>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, onMounted } from "vue";
 import { ThemeMode } from "~~/Domains/Entities/ThemeMode/ThemeMode";
+import ThemeTogglerSpan from "./ThemeTogglerSpan";
 
 export default defineComponent({
+  components: { ThemeTogglerSpan },
   setup() {
     const colorMode = ThemeMode.create();
-    const active = ref(false);
+    const isDarkMode = ref(false);
 
     const toggleTheme = function () {
-      active.value = !active.value;
+      isDarkMode.value = !isDarkMode.value;
       colorMode.toggleMode();
     };
+
+    onMounted(() => {
+      isDarkMode.value = colorMode.isDarkMode;
+      console.log("colorMode.isDarkMode", colorMode.isDarkMode);
+    });
     return {
-      active,
+      isDarkMode,
       toggleTheme,
     };
   },
