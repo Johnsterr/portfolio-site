4987929ed535f17ff125a3e451b215a610d09c44
Evgeniy <evgpashko92@gmail.com>
2022-12-04 22:39:51 +0700

    Базовый компонент Header

---
+++ components/Base/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header
+    class="fixed top-0 left-0 z-[4] w-full transition-all duration-200"
+    :class="classes"
+  >
+    <slot />
+  </header>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    const isScrollToTop = useIsScrollToTop();
+
+    return {
+      isScrollToTop,
+    };
+  },
+  computed: {
+    classes() {
+      return !this.isScrollToTop ? `py-3 bg-bg-secondary` : `py-4`;
+    },
+  },
+});
+</script>
