c39701b9d5548158f74cf091f0b786ad4ccfb277
Evgeniy <evgpashko92@gmail.com>
2022-12-09 23:48:45 +0700

    Header для default шаблона

---
+++ components/Default/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <BaseHeader>
+    <div class="container mx-auto flex items-center justify-between gap-3 px-3">
+      <h1
+        class="relative z-[2] min-w-[80px] text-[2rem] font-normal leading-[1.2]"
+      >
+        <NuxtLink :to="{ name: 'index' }" class="capitalize text-color-primary">
+          Лого<span class="ml-[2px] inline-block text-raw-seinna">.</span>
+        </NuxtLink>
+      </h1>
+
+      <div class="ml-auto flex items-center gap-4">
+        <UIThemeToggler />
+      </div>
+      <!-- Дебаг div -->
+      <div>{{ isScrollToTop }}</div>
+
+      <Menu />
+    </div>
+  </BaseHeader>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    const isScrollToTop = useIsScrollToTop();
+
+    return {
+      isScrollToTop,
+    };
+  },
+});
+</script>
