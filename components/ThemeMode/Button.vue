2b1275bb0c0fd061514d3280c40f567999d98d07
Evgeniy <evgpashko92@gmail.com>
2022-12-15 01:35:25 +0700

    Рефакторинг над кнопкой переключения темы

--- components/ThemeMode/Button.vue
+++ components/ThemeMode/Button.vue
@@ -1,32 +1,34 @@
 <template>
   <button
-    class="h-7 w-[52px] overflow-hidden rounded-[100px] bg-bg-primary p-1 transition-all"
+    class="h-7 w-[52px] overflow-hidden rounded-[100px] bg-bg-secondary p-1 transition-all"
     @click="toggleTheme"
   >
-    <ThemeTogglerSpan :dark-mode-active="isDarkMode" />
+    <ThemeModeButtonSpan :dark-mode-active="isDarkMode" />
   </button>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, onMounted } from "vue";
 import { ThemeMode } from "~~/Domains/Entities/ThemeMode/ThemeMode";
-import ThemeTogglerSpan from "./ThemeTogglerSpan";
 
 export default defineComponent({
-  components: { ThemeTogglerSpan },
   setup() {
     const colorMode = ThemeMode.create();
     const isDarkMode = ref(false);
 
+    function setModeValue(colorMode: ThemeMode) {
+      return colorMode.isDarkMode;
+    }
+
     const toggleTheme = function () {
-      isDarkMode.value = !isDarkMode.value;
       colorMode.toggleMode();
+      isDarkMode.value = setModeValue(colorMode);
     };
 
     onMounted(() => {
-      isDarkMode.value = colorMode.isDarkMode;
-      console.log("colorMode.isDarkMode", colorMode.isDarkMode);
+      isDarkMode.value = setModeValue(colorMode);
     });
+
     return {
       isDarkMode,
       toggleTheme,
---
+++ components/ThemeMode/ButtonSpan.vue
@@ -0,0 +1,23 @@
+<template>
+  <span class="relative top-0 h-5 w-5 transition-all" :class="modeClasses">
+    <IconsMoon v-if="darkModeActive" class="h-5 w-5" />
+    <IconsSun v-else class="h-5 w-5" />
+  </span>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  props: {
+    darkModeActive: {
+      type: Boolean,
+    },
+  },
+  computed: {
+    modeClasses() {
+      return this.darkModeActive
+        ? "left-[22px] text-white"
+        : "left-[2px] text-yellow-500";
+    },
+  },
+});
+</script>
