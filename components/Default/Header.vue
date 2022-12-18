304ea36242cedf82337e1e051840b696123cc02f
Evgeniy <evgpashko92@gmail.com>
2022-12-18 23:48:32 +0700

    Убрал z-index у лого

--- components/Default/Header.vue
+++ components/Default/Header.vue
@@ -1,9 +1,7 @@
 <template>
   <BaseHeader>
     <div class="container mx-auto flex items-center justify-between gap-3 px-3">
-      <h1
-        class="relative z-[2] min-w-[80px] text-[2rem] font-normal leading-[1.2]"
-      >
+      <h1 class="relative min-w-[80px] text-[2rem] font-normal leading-[1.2]">
         <NuxtLink :to="{ name: 'index' }" class="capitalize text-color-primary">
           Лого<span class="ml-[2px] inline-block text-raw-seinna">.</span>
         </NuxtLink>
