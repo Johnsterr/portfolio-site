fe96b870383877c950529443126ff75a977ccc91
Evgeniy <evgpashko92@gmail.com>
2023-01-10 23:43:51 +0700

    Refactor ThemeModeButton

--- components/ThemeMode/Button.vue
+++ components/ThemeMode/Button.vue
@@ -1,22 +1,25 @@
 <template>
-  <button
-    class="h-7 w-[52px] overflow-hidden rounded-[100px] p-1 transition-all"
-    :class="classes"
-    @click="toggleTheme"
-  >
-    <ThemeModeButtonSpan :dark-mode-active="isDarkMode" />
+  <button :class="btnClasses" @click="toggleTheme">
+    <span :class="spanClasses">
+      <IconsMoon v-if="isDarkMode" class="h-5 w-5" />
+      <IconsSun v-else class="h-5 w-5" />
+    </span>
   </button>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted } from "vue";
+import { defineComponent, ref, computed, onMounted } from "vue";
 import { ThemeMode } from "~~/Domains/Entities/ThemeMode/ThemeMode";
 
 export default defineComponent({
   setup() {
+    const baseBtnClasses =
+      "h-7 w-[52px] overflow-hidden rounded-[100px] p-1 transition-all";
+    const baseSpanClasses = "relative top-0 h-5 w-5 transition-all";
+
     const colorMode = ThemeMode.create();
     const isDarkMode = ref(false);
-    const isScrollToTop = useIsScrollToTop();
+    const viewportIsAtTop = useState("viewportIsAtTop");
 
     function setModeValue(colorMode: ThemeMode) {
       return colorMode.isDarkMode;
@@ -27,6 +30,18 @@ export default defineComponent({
       isDarkMode.value = setModeValue(colorMode);
     };
 
+    const btnClasses = computed(() => {
+      let classes = viewportIsAtTop.value ? "bg-bg-secondary" : "bg-bg-primary";
+      return `${baseBtnClasses} ${classes}`;
+    });
+
+    const spanClasses = computed(() => {
+      let classes = isDarkMode.value
+        ? "left-[22px] text-white"
+        : "left-[2px] text-yellow-500";
+      return `${baseSpanClasses} ${classes}`;
+    });
+
     onMounted(() => {
       isDarkMode.value = setModeValue(colorMode);
     });
@@ -34,13 +49,9 @@ export default defineComponent({
     return {
       isDarkMode,
       toggleTheme,
-      isScrollToTop,
+      btnClasses,
+      spanClasses,
     };
-  },
-  computed: {
-    classes() {
-      return this.isScrollToTop ? "bg-bg-secondary" : "bg-bg-primary";
-    },
   },
 });
 </script>
