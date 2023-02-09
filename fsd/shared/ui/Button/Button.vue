ef782e1886bc9cd2f1d9f47818f379b35cfb8012
Evgeniy <evgpashko92@gmail.com>
2023-02-09 23:09:43 +0700

    Prepare button component

---
+++ fsd/shared/ui/Button/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  setup(props, context) {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
